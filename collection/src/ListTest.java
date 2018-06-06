cb0823f2db2e21578682f0f81110f0eea8af0482
jakiejakie <h_j_rose@163.com>
2018-06-06 12:33:14 +0800

    Update ListTest.java

--- collection/src/ListTest.java
+++ collection/src/ListTest.java
@@ -1,6 +1,4 @@
-/**
- * @author 黄景(huangjing@corp.netease.com)
- */
+/** */
 public class ListTest {
     public static void main(String[] args) {
         System.out.println("test create new branch by git");
